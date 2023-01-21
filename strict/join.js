bc20fe76a4da99ab9782a356099248bb8eb30dc0
crowwan <crowwan@naver.com>
2023-01-21 23:27:24 +0900

    add ext

--- strict/join.js
+++ strict/join.js
@@ -1,5 +1,5 @@
-import curry from "./curry";
-import reduce from "./reduce";
+import curry from "./curry.js";
+import reduce from "./reduce.js";
 
 export default curry((sep = ",", iter) =>
   reduce((a, b) => `${a}${sep}${b}`, iter)

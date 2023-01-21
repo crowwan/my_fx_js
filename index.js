f2302341b8edbef1a5745024a87c459986fb2913
crowwan <crowwan@naver.com>
2023-01-21 23:35:31 +0900

    testing

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import go from "./strict/go.js";
 import map from "./strict/map.js";
 import reduce from "./strict/reduce.js";
 import filter from "./strict/filter.js";
-
+import join from "./strict/join.js";
+import take from "./strict/take.js";
 const log = console.log;
 
 go(
   [1, 2, 3, 4, 5, 6, 7, 8],
   map((a) => a * 2),
-  filter((a) => a > 4),
-  reduce((a, b) => a + b),
+  take(4),
   log
 );

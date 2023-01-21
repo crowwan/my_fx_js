5654de28ae352dd9bce930185dd27a96c7c6be68
crowwan <crowwan@naver.com>
2023-01-21 23:12:31 +0900

    change cruyy to curry

--- strict/filter.js
+++ strict/filter.js
@@ -1,4 +1,5 @@
-export default curyy((predicate, iter) => {
+import curry from "./curry.js";
+export default curry((predicate, iter) => {
   const res = [];
   for (const a of iter) if (predicate(a)) res.push(a);
   return res;

9e4acdaffd1af94daee737f5a4756555d57db6d1
crowwan <crowwan@naver.com>
2023-01-22 11:38:51 +0900

    testing

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ import reduce from "./strict/reduce.js";
 import filter from "./strict/filter.js";
 import join from "./strict/join.js";
 import take from "./strict/take.js";
+import mapL from "./lazy/mapL.js";
+import takeL from "./lazy/takeL.js";
+
 const log = console.log;
 
 go(
   [1, 2, 3, 4, 5, 6, 7, 8],
-  map((a) => a * 2),
-  take(4),
+  mapL((a) => a * 2),
+  takeL(4),
+  reduce((a, b) => a + b),
   log
 );
